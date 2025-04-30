ce0b78fb63d34a07b6b7647f1d713f19dff624c9
Alay2305 <164863267+Alay2305@users.noreply.github.com>
2025-04-30 14:49:04 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите два числа");
+int a, b;
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+Console.WriteLine("Сумма чисел");
+Console.WriteLine(a+b);
+Console.WriteLine("Умножение чисел");
+Console.WriteLine(a * b);
